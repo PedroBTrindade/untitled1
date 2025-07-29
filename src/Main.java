2ae2e11822ebf59b95a53a029b6bb7cfd8b70a87
barreto <bb@bb>
2025-07-29 16:49:59 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String oi;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Oiii qual seu nome? ");
+        oi = sc.nextLine();
+        System.out.println("Olá "+ oi );
+    }
+}
